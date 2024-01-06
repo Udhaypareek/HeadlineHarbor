b82a796fa5ca5a26f9937c22895a80265633b25b
Udhay <udhaypareek@gmail.com>
2024-01-06 23:26:56 +0530

    Added Previous and Next Button to render all the news on the page.

--- src/Components/News.js
+++ src/Components/News.js
@@ -6,7 +6,7 @@ export default class News extends Component {
   constructor (){
     super();
     this.state = {
-      articles : this.article,
+      articles : [],
       loading : false,
       page : 1
     }
@@ -19,18 +19,58 @@ export default class News extends Component {
     // console.log(parsedData);
     this.setState({ articles: parsedData.articles })
 }
+handlePrevClick = async () => {
+  console.log("Previous");
+  let url = `https://newsapi.org/v2/top-headlines?country=in&apikey=dbe57b028aeb41e285a226a94865f7a7”&page=${this.state.page - 1}&pageSize=20`;
+  let data = await fetch(url);
+  let parsedData = await data.json()
+  console.log(parsedData);
+  this.setState({
+      page: this.state.page - 1,
+      articles: parsedData.articles
+  })
+}
+  handleNextClick = async () => {
+  console.log("Next");
+  if (this.state.page + 1 > Math.ceil(this.state.totalResults / 20)) {
+
+  }
+  else {
+      let url = `https://newsapi.org/v2/top-headlines?country=in&apikey=dbe57b028aeb41e285a226a94865f7a7&page=${this.state.page + 1}&pageSize=20`;
+      let data = await fetch(url);
+      let parsedData = await data.json()
+      console.log(parsedData);
+      this.setState({
+          page: this.state.page + 1,
+          articles: parsedData.articles
+      })
+  }
+}
   render() {
     return (
         <>
         <div className='container my-3'>
           <h2>Today's Top Headlines</h2>
             <div className="row">
-            {this.state.articles.map((element)=>{
-              return <div className="col-md-4 my-2" key={element.url}>
-                     <Newsitem title ={element.title?element.title.slice(0,70):""} description = {element.description?element.description.slice(0,100):""} imageUrl= {!element.urlToImage?"https://static.feber.se/article_images/57/90/87/579087.jpeg":element.urlToImage} newsUrl = {element.url}/>
-                     </div>
-            })}
+            {this.state.articles && this.state.articles.length > 0 ? (
+            this.state.articles.map((element) => (
+              <div className="col-md-4 my-2" key={element.url}>
+                <Newsitem
+                  title={element.title ? element.title.slice(0, 70) : ""}
+                  description={element.description ? element.description.slice(0, 100) : ""}
+                  imageUrl={!element.urlToImage ? "https://static.feber.se/article_images/57/90/87/579087.jpeg" : element.urlToImage}
+                  newsUrl={element.url}
+                />
+              </div>
+            ))
+          ) : (
+            <p>Loading...</p>
+          )}
             </div>
+              <div className="container d-flex justify-content-between">
+              <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}> &larr;Previous</button>
+              <button type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
+              </div>
         </div>
       </>
     )
--- src/Components/Newsitem.js
+++ src/Components/Newsitem.js
@@ -10,7 +10,7 @@ export default class Newsitem extends Component {
         <div className="card-body">
             <h5 className="card-title">{title}...</h5>
             <p className="card-text">{description}...</p>
-            <a href={newsUrl} target="_blank" className="btn btn-primary">Read more</a>
+            <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-primary">Read more</a>
         </div>
         </div>
       </>
