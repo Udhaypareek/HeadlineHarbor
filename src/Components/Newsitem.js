756805396474962dd39333edbc2d3566484e7c65
Udhay <udhaypareek@gmail.com>
2024-01-06 22:13:22 +0530

    Fetched news through sampleOutput.json using array looping.

--- src/Components/Newsitem.js
+++ src/Components/Newsitem.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react'
 
 export default class Newsitem extends Component {
   render() {
-    let {title , description} = this.props;
+    let {title , description , imageUrl , newsUrl} = this.props;
     return (
       <>
       <div className="card" style={{width: "18rem"}}>
-        <img src="https://cdn.lesnumeriques.com/optim/news/21/217129/ccc335ab-les-voitures-hyundai-et-kia-s-integrent-a-l-ecosysteme-smartthings-de-samsung__1200_630__0-58-1800-1003.jpg" className="card-img-top" alt="..."/>
+        <img src={imageUrl} alt="..."/>
         <div className="card-body">
-            <h5 className="card-title">{title}</h5>
-            <p className="card-text">{description}</p>
-            <a href="/" className="btn btn-primary">Go somewhere</a>
+            <h5 className="card-title">{title}...</h5>
+            <p className="card-text">{description}...</p>
+            <a href={newsUrl} target="_blank" className="btn btn-primary">Read more</a>
         </div>
         </div>
       </>
